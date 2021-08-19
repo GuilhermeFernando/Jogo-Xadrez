c8284879f44941aa7b7d73c55f6ae854a5050900
Guilherme <guifermach@hotmail.com>
2021-08-18 22:00:04 -0300

    Exception personalizada

--- Xadrez-console/Tabuleiro/Tabuleiro.cs
+++ Xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -7,7 +7,7 @@
 
         private Peca[,] pecas;
 
-        public Tabuleiro(int linhas,int colunas)
+        public Tabuleiro(int linhas, int colunas)
         {
             this.linhas = linhas;
             this.colunas = colunas;
@@ -17,14 +17,43 @@
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
+        }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
         }
 
         public void colocarPeca(Peca p, Posicao pos)
         {
             pecas[pos.Linha, pos.Coluna] = p;
             p.posicao = pos;
-
         }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= linhas || pos.Coluna < 0 || pos.Coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if(!posicaoValida (pos))
+            {
+                throw new TabuleiroException("Posição invalida");
+            }
+        }
+
+
     }
 }
